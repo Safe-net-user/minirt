db2d2c9a6f8e58947f94d8bc6713c8afd0b6072d
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-12 19:24:48 +0200

    feat(hdr): add header file to store all macros needed

---
+++ include/macros.h
@@ -0,0 +1,20 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#ifndef STRUCT_ALIGNMENT_64
+# define STRUCT_ALIGNMENT_64 64
+#endif
+
+#ifndef STRUCT_ALIGNMENT_32
+# define STRUCT_ALIGNMENT_32 32
+#endif
+
+#ifndef WINDOW_WIDTH
+# define WINDOW_WIDTH 4096
+#endif
+
+#ifndef WINDOW_HEIGHT
+# define Window_HEIGHT 2048
+#endif
+
+#endif //MINIRT_MACROS_H

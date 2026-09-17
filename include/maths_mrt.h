fe7cf68c7c4deefe6f7316306e9bb283ef483c6b
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 14:31:12 +0200

    feat(header): expose intern math headers

---
+++ include/maths_mrt.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+# include "../src/math/matrix/matrix2.h"
+# include "../src/math/matrix/maths_mrt.h"
+# include "../src/math/matrix/matrix4.h"
+# include "../src/math/point.h"
+# include "../src/math/vec4.h"
+
+#endif //MATRIX_H

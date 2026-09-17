a9fb3ff47a39164feb43b977f988fd0f077fa432
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 14:38:45 +0200

    fix: change macro protection name

--- include/maths.h
+++ include/maths.h
@@ -1,5 +1,5 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#ifndef MATHS_H
+#define MATHS_H
 
 # include "../src/math/matrix/matrix2.h"
 # include "../src/math/matrix/matrice3.h"
@@ -7,4 +7,4 @@
 # include "../src/math/point.h"
 # include "../src/math/vec4.h"
 
-#endif //MATRIX_H
+#endif //MATHS_H

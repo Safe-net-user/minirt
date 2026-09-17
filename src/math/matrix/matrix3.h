2a2ba8f44f2b2f84674aa23371bd91313b30007e
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 16:37:34 +0200

    feat: expose compute_determinant function

--- src/math/matrix/matrix3.h
+++ src/math/matrix/matrix3.h
@@ -12,6 +12,6 @@ void	submatrix3(t_m2 *mf, t_m3, int i, int j);
 double	compute_minor3(t_m3 m, int i, int j);
 double	compute_cofactors3(t_m3 m, int i, int j);
 double	compute_minor3(t_m3 m, int i, int j);
-double	compute_determinant3(const t_m3 m);
+double	compute_determinant3(t_m3 m);
 
 #endif

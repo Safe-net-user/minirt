06125cf61087d2d668a6fdc8a3ec646f873f4aaa
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 17:04:48 +0200

    fix(header): change include path of matrix3

--- include/maths.h
+++ include/maths.h
@@ -2,7 +2,7 @@
 #define MATHS_H
 
 # include "../src/math/matrix/matrix2.h"
-# include "../src/math/matrix/matrice3.h"
+# include "../src/math/matrix/matrix3.h"
 # include "../src/math/matrix/matrix4.h"
 # include "../src/math/point.h"
 # include "../src/math/vec4.h"

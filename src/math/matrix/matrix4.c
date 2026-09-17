1c0a41a2f6e4c8df83777a752c836c16fb5bf6f0
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 16:38:49 +0200

    fix(matrix): function arguments define as constant

--- src/math/matrix/matrix4.c
+++ src/math/matrix/matrix4.c
@@ -1,8 +1,7 @@
 #include "matrix4.h"
 #include "matrix3.h"
-#include "matrix2.h"
 
-void	submatrix4(t_m3 *mf, t_m4 m, int i, int j)
+void	submatrix4(t_m3 *mf, const t_m4 m, const int i, const int j)
 {
 	int	k;
 	int	l;
@@ -26,15 +25,15 @@ void	submatrix4(t_m3 *mf, t_m4 m, int i, int j)
 	}
 }
 
-double	compute_minor4(t_m4 m, int i, int j)
+double	compute_minor4(const t_m4 m, const int i, const int j)
 {
 	t_m3	m3;
 
-	submatrix4(&m3, i, j);
+	submatrix4(&m3, m, i, j);
 	return (compute_determinant3(m3));
 }
 
-double	compute_cofactors4(t_m4 m, int i, int j)
+double	compute_cofactors4(const t_m4 m, const int i, const int j)
 {
 	double	minor;
 
@@ -44,12 +43,12 @@ double	compute_cofactors4(t_m4 m, int i, int j)
 	return (minor);
 }
 
-double	compute_determinant4(t_m4 m)
+double	compute_determinant4(const t_m4 m)
 {
 	return (m.d[0] * compute_cofactors4(m, 0, 0) + m.d[1] * compute_cofactors4(m, 0, 1) + m.d[2] * compute_cofactors4(m, 0, 2) + m.d[3] * compute_cofactors4(m, 0, 3));
 }
 
-void	transpose_matrices4(t_m4 *fm, t_m4 m)
+void	transpose_matrices4(t_m4 *fm, const t_m4 m)
 {
 	fm->d[0] = m.d[0];
 	fm->d[1] = m.d[4];
@@ -69,7 +68,7 @@ void	transpose_matrices4(t_m4 *fm, t_m4 m)
 	fm->d[15] = m.d[15];
 }
 
-void	mul_matrices4(t_m4 *fm, t_m4 m1, t_m4 m2)
+void	mul_matrices4(t_m4 *fm, const t_m4 m1, const t_m4 m2)
 {
 	fm->d[0] = m1.d[0] * m2.d[0] + m1.d[1] * m2.d[4] + m1.d[2] * m2.d[8] + m1.d[3] * m2.d[12];
 	fm->d[1] = m1.d[0] * m2.d[1] + m1.d[1] * m2.d[5] + m1.d[2] * m2.d[9] + m1.d[3] * m2.d[13];
@@ -87,4 +86,29 @@ void	mul_matrices4(t_m4 *fm, t_m4 m1, t_m4 m2)
 	fm->d[13] = m1.d[12] * m2.d[1] + m1.d[13] * m2.d[5] + m1.d[14] * m2.d[9] + m1.d[15] * m2.d[13];
 	fm->d[14] = m1.d[12] * m2.d[2] + m1.d[13] * m2.d[6] + m1.d[14] * m2.d[10] + m1.d[15] * m2.d[14];
 	fm->d[15] = m1.d[12] * m2.d[3] + m1.d[13] * m2.d[7] + m1.d[14] * m2.d[11] + m1.d[15] * m2.d[15];
+}
+
+void	invert_matrix4(t_m4 *fm, const t_m4 m)
+{
+	double	det;
+	int		i;
+	int		j;
+
+	if (!compute_determinant4(m))
+	{
+		fm = NULL;
+		return ;
+	}
+	det = compute_determinant4(m);
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			fm->d[i + j] = compute_cofactors4(m, j, i) / det;
+			j++;
+		}
+		i++;
+	}
 }

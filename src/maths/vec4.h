41085fe44bdcc1e4bd3bf20e1c8e0a8e4a8007ba
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-22 13:37:34 +0200

    feat(hdr): add vector fucntion added

--- src/maths/vec4.h
+++ src/maths/vec4.h
@@ -11,7 +11,7 @@ typedef struct s_vec4
 	double	w;
 }	t_vec4;
 
-static  __attribute__((__always_inline__)) void set_vec4(t_vec4 *v, double x, double y, double z)
+static  inline __attribute__((__always_inline__)) void set_vec4(t_vec4 *v, const double x, const double y, const double z)
 {
 	v->x = x;
 	v->y = y;
@@ -19,7 +19,15 @@ static  __attribute__((__always_inline__)) void set_vec4(t_vec4 *v, double x, do
 	v->w = 0.0;
 }
 
-static __attribute__((__always_inline__)) void	sub_vectors(t_vec4 *fv, t_vec4 v1, t_vec4 v2)
+static inline __attribute__((__always_inline__)) void	add_vectors(t_vec4 *fv, const t_vec4 v1, const t_vec4 v2)
+{
+	fv->x = v1.x + v2.x;
+	fv->y = v1.y + v2.y;
+	fv->z = v1.z + v2.z;
+	fv->w = v1.w + v2.w;
+}
+
+static inline __attribute__((__always_inline__)) void	sub_vectors(t_vec4 *fv, const t_vec4 v1, const t_vec4 v2)
 {
 	fv->x = v1.x - v2.x;
 	fv->y = v1.y - v2.y;
@@ -27,7 +35,7 @@ static __attribute__((__always_inline__)) void	sub_vectors(t_vec4 *fv, t_vec4 v1
 	fv->w = v1.w - v2.w;
 }
 
-static __attribute__((__always_inline__)) void	negate_vector(t_vec4 *v)
+static inline __attribute__((__always_inline__)) void	negate_vector(t_vec4 *v)
 {
 	v->x = -v->x;
 	v->y = -v->y;
@@ -35,7 +43,7 @@ static __attribute__((__always_inline__)) void	negate_vector(t_vec4 *v)
 	v->w = -v->w;
 }
 
-static __attribute__((always_inline)) void	mul_vector(t_vec4 *v, double s)
+static inline __attribute__((always_inline)) void	mul_vector(t_vec4 *v, const double s)
 {
 	v->x *= s;
 	v->y *= s;
@@ -43,12 +51,12 @@ static __attribute__((always_inline)) void	mul_vector(t_vec4 *v, double s)
 	v->w *= s;
 }
 
-static __attribute__((always_inline)) double	compute_magnitude(t_vec4 v)
+static inline __attribute__((always_inline)) double	compute_magnitude(const t_vec4 v)
 {
 	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w));
 }
 
-static __attribute__((always_inline)) void	normalize_vector(t_vec4 *v, double magnitude)
+static inline __attribute__((always_inline)) void	normalize_vector(t_vec4 *v, const double magnitude)
 {
 	double	s;
 
@@ -58,12 +66,12 @@ static __attribute__((always_inline)) void	normalize_vector(t_vec4 *v, double ma
 	mul_vector(v, s);
 }
 
-static __attribute__((always_inline)) double	compute_dot(t_vec4 v1, t_vec4 v2)
+static inline __attribute__((always_inline)) double	compute_dot(const t_vec4 v1, const t_vec4 v2)
 {
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w);
 }
 
-static __attribute__((always_inline)) t_vec4	*compute_cross(t_vec4 *vf, t_vec4 v1, t_vec4 v2)
+static inline __attribute__((always_inline)) t_vec4	*compute_cross(t_vec4 *vf, const t_vec4 v1, const t_vec4 v2)
 {
 	set_vec4(
 		vf,
